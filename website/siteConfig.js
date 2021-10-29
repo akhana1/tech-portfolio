c598b2fe7d5550340bb23e37c3599a34ea70294e
akhana1 <59046152+akhana1@users.noreply.github.com>
2021-10-29 17:38:25 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -69,7 +69,7 @@ const siteConfig = {
     socialMedia: {
         linkedin: 'ashish-khanal-b5b9451a5',
         twitter: '@Ashish__Khanal',
-        github: 'akhanal1',
+        github: 'akhanal01',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
